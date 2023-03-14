c3cf3f73ecd7bbb859c3710c06cb97862bf1ece4
Don Jayamanne <don.jayamanne@outlook.com>
2023-03-15 06:21:20 +1100

    Download specific platform files (#6)
    * Download specific platform files
    
    * Minimal tests
    
    * Update readme

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export declare function downloadZMQ(): Promise<void>;
+export type Options = {
+  win32?: ("x64" | "ia32" | "arm64")[];
+  linux?: ("x64" | "arm64" | "armhf")[];
+  darwin?: ("x64" | "arm64")[];
+  alpine?: ("x64" | "arm64")[];
+};
+
+/**
+ * Downloads the ZMQ binaries.
+ *
+ * @param {Options} options If not provided, then downloads all binaries for all platforms + archs.
+ * @return {*}  {Promise<void>}
+ */
+export declare function downloadZMQ(options: Options): Promise<void>;
