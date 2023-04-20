0e353d1ff27a74b2956af428d99195833e6abdb4
Don Jayamanne <don.jayamanne@outlook.com>
2023-04-20 16:46:14 +1000

    Ship binaries with npm package (#16)
    * Ship binaries with npm package
    
    * more changes

---
+++ build/prePublish.js
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+"use strict";
+
+const path = require("path");
+const { download } = require("./download");
+const fs = require("fs");
+
+const VERSION = "6.0.0-beta.16.5";
+
+/**
+ * Downloads the ZMQ binaries.
+ */
+async function downloadZMQ() {
+  const destination = path.join(__dirname, "..", "prebuilds");
+  if (fs.existsSync(path.dirname(destination))) {
+    fs.rmSync(destination, { force: true, recursive: true });
+  }
+  fs.mkdirSync(destination);
+  const downloadOptions = {
+    version: VERSION,
+    token: process.env["GITHUB_TOKEN"],
+    destination,
+    force: true,
+  };
+  await download(downloadOptions);
+}
+
+downloadZMQ()
+  .then(() => process.exit(0))
+  .catch((ex) => {
+    console.error("Failed to download ZMQ", ex);
+    process.exit(1);
+  });
