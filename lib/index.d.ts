1079c9c266062cce2ed1776995a6bb26f78bb258
Don Jayamanne <don.jayamanne@outlook.com>
2023-04-23 10:18:35 +1000

    Package armhf and alpine-arm64

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export type Options = {
-  win32?: ("x64" | "ia32" | "arm64")[];
-  linux?: ("x64" | "arm64" | "armhf" | "arm")[];
-  darwin?: ("x64" | "arm64")[];
-  alpine?: ("x64" | "arm64")[];
-};
-
 /**
  * Downloads the ZMQ binaries.
  *
- * @param {Options} options If not provided, then downloads all binaries for all platforms + archs.
  * @return {*}  {Promise<void>}
  */
-export declare function downloadZMQ(options: Options): Promise<void>;
+export declare function downloadZMQ(): Promise<void>;
